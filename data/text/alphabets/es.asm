3fd9b02f27c8979c47fc216c236c566ba76001bb
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 06:35:07 +0900

    Add Spanish naming alphabet table from reference ROM

---
+++ data/text/alphabets/es.asm
@@ -0,0 +1,15 @@
+LowerCaseAlphabet:
+	db "abcdefghi"
+	db "jklmnopqr"
+	db "stuvwxyz "
+	db "×():;[]<PK><MN>"
+	db "-?!♂♀/<DOT>,<ED>"
+	db "MAY", $ce, "SCULAS@" ; Ú = $ce in the Spanish Gen I charmap
+
+UpperCaseAlphabet:
+	db "ABCDEFGHI"
+	db "JKLMNOPQR"
+	db "STUVWXYZ "
+	db "×():;[]<PK><MN>"
+	db "-?!♂♀/<DOT>,<ED>"
+	db "min", $d5, "sculas@" ; ú = $d5 in the Spanish Gen I charmap
