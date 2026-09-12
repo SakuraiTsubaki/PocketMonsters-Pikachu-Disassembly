b919e359d80bbe4ff810b2e49f6a0b35a084b354
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 04:06:10 +0900

    Reconstruct overworld tileset and connection-map core

---
+++ home/overworld/tilemap_connections.asm
@@ -0,0 +1,400 @@
+	dec a
+	jr z, .ridingBike
+
+	ldh a, [hTileAnimations]
+	and a
+	jr nz, .determineGraphics
+	jr .startWalking
+
+.ridingBike
+	; If the bike can't be used,
+	; start walking instead.
+	call IsBikeRidingAllowed
+	jr c, .determineGraphics
+
+.startWalking
+	xor a
+	ld [wWalkBikeSurfState], a
+	ld [wWalkBikeSurfStateCopy], a
+	jp LoadWalkingPlayerSpriteGraphics
+
+.determineGraphics
+	ld a, [wWalkBikeSurfState]
+	and a
+	jp z, LoadWalkingPlayerSpriteGraphics
+	dec a
+	jp z, LoadBikePlayerSpriteGraphics
+	dec a
+	jp z, LoadSurfingPlayerSpriteGraphics2
+	jp LoadWalkingPlayerSpriteGraphics
+
+IsBikeRidingAllowed::
+; The bike can be used on Route 23 and Indigo Plateau,
+; or maps with tilesets in BikeRidingTilesets.
+; Return carry if biking is allowed.
+
+	ld a, [wCurMap]
+	cp ROUTE_23
+	jr z, .allowed
+	cp INDIGO_PLATEAU
+	jr z, .allowed
+
+	ld a, [wCurMapTileset]
+	ld b, a
+	ld hl, BikeRidingTilesets
+.loop
+	ld a, [hli]
+	cp b
+	jr z, .allowed
+	inc a
+	jr nz, .loop
+	and a
+	ret
+
+.allowed
+	scf
+	ret
+
+INCLUDE "data/tilesets/bike_riding_tilesets.asm"
+
+; load the tile pattern data of the current tileset into VRAM
+LoadTilesetTilePatternData::
+	ld a, [wTilesetGfxPtr]
+	ld l, a
+	ld a, [wTilesetGfxPtr + 1]
+	ld h, a
+	ld de, vTileset
+	ld bc, MAP_TILESET_SIZE tiles
+	ld a, [wTilesetBank]
+	jp FarCopyData
+
+; this loads the current map's complete tile map (which references blocks, not individual tiles) to wOverworldMap
+; it can also load partial tile maps of connected maps into a border of length 3 around the current map
+LoadTileBlockMap::
+; fill wOverworldMap-wOverworldMapEnd with the background tile
+	ld hl, wOverworldMap
+	ld bc, wOverworldMapEnd - wOverworldMap
+	ld a, [wMapBackgroundTile] ; background tile number
+	call FillMemory
+; load tile map of current map (made of tile block IDs)
+; a 3-byte border at the edges of the map is kept so that there is space for map connections
+	ld hl, wOverworldMap
+	ld a, [wCurMapWidth]
+	ldh [hMapWidth], a
+	add MAP_BORDER * 2 ; east and west
+	ldh [hMapStride], a ; map width + border
+	ld b, 0
+	ld c, a
+; make space for north border (next 3 lines)
+	add hl, bc
+	add hl, bc
+	add hl, bc
+	ld c, MAP_BORDER
+	add hl, bc ; this puts us past the (west) border
+	ld a, [wCurMapDataPtr] ; tile map pointer
+	ld e, a
+	ld a, [wCurMapDataPtr + 1]
+	ld d, a ; de = tile map pointer
+	ld a, [wCurMapHeight]
+	ld b, a
+.rowLoop ; copy one row each iteration
+	push hl
+	ldh a, [hMapWidth] ; map width (without border)
+	ld c, a
+.rowInnerLoop
+	ld a, [de]
+	inc de
+	ld [hli], a
+	dec c
+	jr nz, .rowInnerLoop
+; add the map width plus the border to the base address of the current row to get the next row's address
+	pop hl
+	ldh a, [hMapStride] ; map width + border
+	add l
+	ld l, a
+	jr nc, .noCarry
+	inc h
+.noCarry
+	dec b
+	jr nz, .rowLoop
+; north connection
+	ld a, [wNorthConnectedMap]
+	cp $ff
+	jr z, .southConnection
+	call SwitchToMapRomBank
+	ld a, [wNorthConnectionStripSrc]
+	ld l, a
+	ld a, [wNorthConnectionStripSrc + 1]
+	ld h, a
+	ld a, [wNorthConnectionStripDest]
+	ld e, a
+	ld a, [wNorthConnectionStripDest + 1]
+	ld d, a
+	ld a, [wNorthConnectionStripLength]
+	ldh [hNorthSouthConnectionStripWidth], a
+	ld a, [wNorthConnectedMapWidth]
+	ldh [hNorthSouthConnectedMapWidth], a
+	call LoadNorthSouthConnectionsTileMap
+.southConnection
+	ld a, [wSouthConnectedMap]
+	cp $ff
+	jr z, .westConnection
+	call SwitchToMapRomBank
+	ld a, [wSouthConnectionStripSrc]
+	ld l, a
+	ld a, [wSouthConnectionStripSrc + 1]
+	ld h, a
+	ld a, [wSouthConnectionStripDest]
+	ld e, a
+	ld a, [wSouthConnectionStripDest + 1]
+	ld d, a
+	ld a, [wSouthConnectionStripLength]
+	ldh [hNorthSouthConnectionStripWidth], a
+	ld a, [wSouthConnectedMapWidth]
+	ldh [hNorthSouthConnectedMapWidth], a
+	call LoadNorthSouthConnectionsTileMap
+.westConnection
+	ld a, [wWestConnectedMap]
+	cp $ff
+	jr z, .eastConnection
+	call SwitchToMapRomBank
+	ld a, [wWestConnectionStripSrc]
+	ld l, a
+	ld a, [wWestConnectionStripSrc + 1]
+	ld h, a
+	ld a, [wWestConnectionStripDest]
+	ld e, a
+	ld a, [wWestConnectionStripDest + 1]
+	ld d, a
+	ld a, [wWestConnectionStripLength]
+	ld b, a
+	ld a, [wWestConnectedMapWidth]
+	ldh [hEastWestConnectedMapWidth], a
+	call LoadEastWestConnectionsTileMap
+.eastConnection
+	ld a, [wEastConnectedMap]
+	cp $ff
+	jr z, .done
+	call SwitchToMapRomBank
+	ld a, [wEastConnectionStripSrc]
+	ld l, a
+	ld a, [wEastConnectionStripSrc + 1]
+	ld h, a
+	ld a, [wEastConnectionStripDest]
+	ld e, a
+	ld a, [wEastConnectionStripDest + 1]
+	ld d, a
+	ld a, [wEastConnectionStripLength]
+	ld b, a
+	ld a, [wEastConnectedMapWidth]
+	ldh [hEastWestConnectedMapWidth], a
+	call LoadEastWestConnectionsTileMap
+.done
+	ret
+
+LoadNorthSouthConnectionsTileMap::
+	ld c, MAP_BORDER
+.loop
+	push de
+	push hl
+	ldh a, [hNorthSouthConnectionStripWidth]
+	ld b, a
+.innerLoop
+	ld a, [hli]
+	ld [de], a
+	inc de
+	dec b
+	jr nz, .innerLoop
+	pop hl
+	pop de
+	ldh a, [hNorthSouthConnectedMapWidth]
+	add l
+	ld l, a
+	jr nc, .noCarry1
+	inc h
+.noCarry1
+	ld a, [wCurMapWidth]
+	add MAP_BORDER * 2
+	add e
+	ld e, a
+	jr nc, .noCarry2
+	inc d
+.noCarry2
+	dec c
+	jr nz, .loop
+	ret
+
+LoadEastWestConnectionsTileMap::
+	push hl
+	push de
+	ld c, MAP_BORDER
+.innerLoop
+	ld a, [hli]
+	ld [de], a
+	inc de
+	dec c
+	jr nz, .innerLoop
+	pop de
+	pop hl
+	ldh a, [hEastWestConnectedMapWidth]
+	add l
+	ld l, a
+	jr nc, .noCarry1
+	inc h
+.noCarry1
+	ld a, [wCurMapWidth]
+	add MAP_BORDER * 2
+	add e
+	ld e, a
+	jr nc, .noCarry2
+	inc d
+.noCarry2
+	dec b
+	jr nz, LoadEastWestConnectionsTileMap
+	ret
+
+; function to check if there is a sign or sprite in front of the player
+; if so, it is stored in [hTextID]
+; if not, [hTextID] is set to 0
+IsSpriteOrSignInFrontOfPlayer::
+	xor a
+	ldh [hTextID], a
+	ld a, [wNumSigns]
+	and a
+	jr z, .extendRangeOverCounter
+; if there are signs
+	predef GetTileAndCoordsInFrontOfPlayer ; get the coordinates in front of the player in de
+	call SignLoop
+	ret c
+.extendRangeOverCounter
+; check if the player is front of a counter in a pokemon center, pokemart, etc. and if so, extend the range at which he can talk to the NPC
+	predef GetTileAndCoordsInFrontOfPlayer ; get the tile in front of the player in c
+	ld hl, wTilesetTalkingOverTiles ; list of tiles that extend talking range (counter tiles)
+	ld b, 3
+	ld d, $20 ; talking range in pixels (long range)
+.counterTilesLoop
+	ld a, [hli]
+	cp c
+	jr z, IsSpriteInFrontOfPlayer2 ; jumps if the tile in front of the player is a counter tile
+	dec b
+	jr nz, .counterTilesLoop
+
+; part of the above function, but sometimes its called on its own, when signs are irrelevant
+; the caller must zero [hTextID]
+IsSpriteInFrontOfPlayer::
+	ld d, $10 ; talking range in pixels (normal range)
+IsSpriteInFrontOfPlayer2::
+	lb bc, $3c, $40 ; Y and X position of player sprite
+	ld a, [wSpritePlayerStateData1FacingDirection]
+; check if player facing up
+	cp SPRITE_FACING_UP
+	jr nz, .checkIfPlayerFacingDown
+; facing up
+	ld a, b
+	sub d
+	ld b, a
+	ld a, PLAYER_DIR_UP
+	jr .doneCheckingDirection
+
+.checkIfPlayerFacingDown
+	cp SPRITE_FACING_DOWN
+	jr nz, .checkIfPlayerFacingRight
+; facing down
+	ld a, b
+	add d
+	ld b, a
+	ld a, PLAYER_DIR_DOWN
+	jr .doneCheckingDirection
+
+.checkIfPlayerFacingRight
+	cp SPRITE_FACING_RIGHT
+	jr nz, .playerFacingLeft
+; facing right
+	ld a, c
+	add d
+	ld c, a
+	ld a, PLAYER_DIR_RIGHT
+	jr .doneCheckingDirection
+
+.playerFacingLeft
+; facing left
+	ld a, c
+	sub d
+	ld c, a
+	ld a, PLAYER_DIR_LEFT
+.doneCheckingDirection
+	ld [wPlayerDirection], a
+	ld hl, wSprite01StateData1
+; yellow does not have the "if sprites are existent" check
+	ld e, $01
+	ld d, $f
+.spriteLoop
+	push hl
+	ld a, [hli] ; image (0 if no sprite)
+	and a
+	jr z, .nextSprite
+	inc l
+	ld a, [hli] ; sprite visibility
+	inc a
+	jr z, .nextSprite
+	inc l
+	ld a, [hli] ; Y location
+	cp b
+	jr nz, .nextSprite
+	inc l
+	ld a, [hl] ; X location
+	cp c
+	jr z, .foundSpriteInFrontOfPlayer
+.nextSprite
+	pop hl
+	ld a, l
+	add SPRITESTATEDATA1_LENGTH
+	ld l, a
+	inc e
+	dec d
+	jr nz, .spriteLoop
+	xor a
+	ret
+
+.foundSpriteInFrontOfPlayer
+	pop hl
+	ld a, l
+	and $f0
+	inc a
+	ld l, a ; hl = x#SPRITESTATEDATA1_MOVEMENTSTATUS
+	set BIT_FACE_PLAYER, [hl]
+	ld a, e
+	ldh [hSpriteIndex], a
+	ldh a, [hSpriteIndex] ; possible useless read because a already has the value of the read address
+	cp PIKACHU_SPRITE_INDEX
+	jr nz, .dontwritetowd436
+	ld a, $FF
+	ld [wd435], a
+.dontwritetowd436
+	scf
+	ret
+
+SignLoop::
+; search if a player is facing a sign
+	ld hl, wSignCoords
+	ld a, [wNumSigns]
+	ld b, a
+	ld c, 0
+.signLoop
+	inc c
+	ld a, [hli] ; sign Y
+	cp d
+	jr z, .yCoordMatched
+	inc hl
+	jr .retry
+.yCoordMatched
+	ld a, [hli] ; sign X
+	cp e
+	jr nz, .retry
+; X coord matched: found sign
+	push hl
+	push bc
+	ld hl, wSignTextIDs
+	ld b, 0
+	dec c
+	add hl, bc
