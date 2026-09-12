d4d9526bbc4fdeafd504dddc78a1406ee9a2f55d
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 06:35:18 +0900

    Add Japanese naming kana table

---
+++ data/text/kana.asm
@@ -0,0 +1,20 @@
+HiraganaCharacters:
+	db "あかさたなはまやら"
+	db "いきしちにひみゆり"
+	db "うくすつぬふむよる"
+	db "えけせてねへめわれ"
+	db "おこそとのほもんろ"
+	db "ゃゅょっ゛゜ー　<ED>"
+	db "カナ@"
+
+KatakanaCharacters:
+	db "アカサタナハマヤラ"
+	db "イキシチニヒミユリ"
+	db "ウクスツヌフムヨル"
+	db "エケセテネヘメワレ"
+	db "オコソトノホモンロ"
+	db "ャュョッ゛゜ー　<ED>"
+	db "かな@"
+
+KanaInputText:
+	db "にゅうりょく@"
