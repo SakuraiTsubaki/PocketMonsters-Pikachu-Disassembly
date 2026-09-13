616cbefa94f7a0cef7d5049658c3b56cc7cc24f3
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 10:17:50 +0900

    audio: add fifth Bank 02 cry source batch

---
+++ audio/sfx/cry19.asm
@@ -0,0 +1,21 @@
+	audio_eng_channel SFX_Cry19, 5
+
+	duty_cycle_pattern 0, 1, 2, 3
+	square_note 7, 13, 2, 1856
+	square_note 15, 14, 5, 1888
+	square_note 15, 12, 1, 1840
+	sound_ret
+
+
+	audio_eng_channel SFX_Cry19, 6
+
+	duty_cycle_pattern 2, 0, 0, 1
+	square_note 2, 12, 2, 1793
+	square_note 4, 12, 2, 1800
+	square_note 15, 13, 7, 1857
+	square_note 15, 10, 2, 1793
+
+
+	audio_eng_channel SFX_Cry19, 8
+
+	sound_ret
