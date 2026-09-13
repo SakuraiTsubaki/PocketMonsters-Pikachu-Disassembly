2cabf1fe77a8a7f66cb8ce0328e400125a9af347
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:49:41 +0900

    audio: reconstruct Fly SFX

---
+++ audio/sfx/fly.asm
@@ -0,0 +1,19 @@
+	audio_eng_channel SFX_Fly, 8
+
+	noise_note 2, 15, 1, 18
+	noise_note 2, 0, 0, 0
+	noise_note 2, 10, 1, 18
+	noise_note 2, 0, 0, 0
+	noise_note 2, 13, 1, 18
+	noise_note 2, 0, 0, 0
+	noise_note 2, 8, 1, 18
+	noise_note 2, 0, 0, 0
+	noise_note 2, 11, 1, 18
+	noise_note 2, 0, 0, 0
+	noise_note 2, 6, 1, 18
+	noise_note 2, 0, 0, 0
+	noise_note 2, 9, 1, 18
+	noise_note 2, 0, 0, 0
+	noise_note 2, 4, 1, 18
+	noise_note 2, 0, 0, 0
+	sound_ret
