b3b495fee2ec7418f2838cb7c5744430e8ca4113
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:35:52 +0900

    bank02: add Music 1 header table

---
+++ audio/headers/music_headers_1.asm
@@ -0,0 +1,22 @@
+; Engine 1 music pointers.
+
+audio_header Music_PalletTown,    1, 2, 3
+audio_header Music_Pokecenter,    1, 2, 3
+audio_header Music_Gym,           1, 2, 3
+audio_header Music_Cities1,       1, 2, 3, 4
+audio_header Music_Cities2,       1, 2, 3
+audio_header Music_Celadon,       1, 2, 3
+audio_header Music_Cinnabar,      1, 2, 3
+audio_header Music_Vermilion,     1, 2, 3, 4
+audio_header Music_Lavender,      1, 2, 3, 4
+audio_header Music_SSAnne,        1, 2, 3
+audio_header Music_MeetProfOak,   1, 2, 3
+audio_header Music_MeetRival,     1, 2, 3
+audio_header Music_MuseumGuy,     1, 2, 3, 4
+audio_header Music_SafariZone,    1, 2, 3
+audio_header Music_PkmnHealed,    1, 2, 3
+audio_header Music_Routes1,       1, 2, 3, 4
+audio_header Music_Routes2,       1, 2, 3, 4
+audio_header Music_Routes3,       1, 2, 3, 4
+audio_header Music_Routes4,       1, 2, 3, 4
+audio_header Music_IndigoPlateau, 1, 2, 3, 4
