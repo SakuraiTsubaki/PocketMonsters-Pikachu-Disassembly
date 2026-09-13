bc49ff2014d2e04c269ef388ed507403e1e37d21
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:44:59 +0900

    audio: preserve unused SFX 59

---
+++ audio/sfx/59.asm
@@ -0,0 +1,12 @@
+	audio_eng_channel SFX_59, 5
+
+	duty_cycle 2
+	square_note 4, 15, 1, 1920
+	sound_ret
+
+	audio_eng_channel SFX_59, 6
+
+	duty_cycle 2
+	square_note 1, 0, 8, 0
+	square_note 4, 10, 1, 1889
+	sound_ret
