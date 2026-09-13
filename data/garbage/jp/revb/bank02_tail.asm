b297b6b797905c257d81c9b7fb09e4fd0230e623
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 10:09:19 +0900

    audio: preserve JP Rev B Bank 02 garbage tail as ASM

---
+++ data/garbage/jp/revb/bank02_tail.asm
@@ -0,0 +1,26 @@
+; Pocket Monsters Pikachu JP Rev B — Bank 02 historical Garbage 2
+; ROM range: $BEC7-$BFFF (313 bytes)
+; Raw SHA-1: 6bd1fed0b4151c0a4a47ac75dcea5850002f813f
+; Git blob SHA-1: 527ecf5802e41e223919bd4233f425bc37403802
+; Preserved verbatim from the verified reference ROM; do not normalize as padding.
+
+	db $d2, $84, $4a, $95, $13, $84, $84, $11, $59, $00, $09, $10, $1a, $03, $a1, $10
+	db $20, $90, $ec, $11, $83, $e8, $80, $03, $30, $00, $88, $c0, $00, $28, $41, $10
+	db $02, $a0, $00, $90, $10, $60, $fd, $16, $11, $46, $a2, $9c, $53, $04, $44, $10
+	db $10, $40, $91, $02, $40, $47, $15, $83, $ab, $ff, $ff, $bf, $ef, $ef, $ff, $ef
+	db $db, $ff, $df, $ff, $ff, $fd, $df, $ff, $ef, $fd, $ff, $ff, $f9, $ff, $ff, $df
+	db $ef, $bf, $bf, $ff, $bf, $7f, $ef, $ff, $ff, $ff, $ad, $ef, $ef, $db, $ff, $5f
+	db $ff, $ff, $bf, $ff, $7b, $ff, $6f, $ff, $ff, $7f, $bf, $ff, $ff, $ff, $ff, $fe
+	db $ff, $ff, $ff, $ff, $fd, $f6, $bf, $df, $7f, $fb, $bf, $df, $f6, $7f, $ff, $df
+	db $ef, $ff, $f7, $ff, $ff, $ed, $bf, $fb, $ff, $ff, $da, $ef, $ff, $ff, $ff, $ff
+	db $ef, $e7, $ff, $df, $ed, $eb, $ff, $ff, $7f, $fe, $ff, $ff, $ed, $ff, $f5, $fe
+	db $ff, $df, $ff, $ed, $f3, $ff, $ef, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $df
+	db $fd, $ff, $ef, $ff, $bf, $fd, $fc, $ff, $ff, $00, $40, $8c, $80, $a1, $31, $28
+	db $81, $41, $02, $00, $88, $01, $00, $40, $8b, $91, $b8, $82, $a5, $41, $43, $08
+	db $00, $41, $a2, $81, $0c, $11, $68, $e1, $94, $00, $00, $81, $91, $02, $0b, $0d
+	db $db, $21, $0f, $21, $50, $10, $18, $19, $e9, $03, $08, $04, $05, $00, $81, $02
+	db $4c, $01, $11, $f2, $06, $04, $84, $08, $e4, $00, $c0, $03, $90, $40, $03, $24
+	db $81, $01, $21, $1c, $0b, $01, $33, $09, $02, $00, $40, $01, $80, $23, $84, $04
+	db $07, $01, $40, $33, $04, $82, $48, $40, $90, $00, $28, $88, $91, $21, $00, $2d
+	db $41, $08, $43, $0e, $20, $09, $11, $01, $08, $29, $3c, $21, $80, $40, $38, $42
+	db $40, $20, $02, $48, $21, $00, $5f, $09, $12
