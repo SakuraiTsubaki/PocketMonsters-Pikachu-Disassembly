deb5811b5a347be70d5623be05891fb2e51319e4
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:46:32 +0900

    audio: reconstruct Heal Ailment SFX

---
+++ audio/sfx/heal_ailment.asm
@@ -0,0 +1,10 @@
+	audio_eng_channel SFX_Heal_Ailment, 5
+
+	duty_cycle 2
+	pitch_sweep 1, 4
+	square_note 4, 15, 2, 1536
+	square_note 4, 15, 2, 1536
+	pitch_sweep 1, 7
+	square_note 15, 15, 2, 1536
+	pitch_sweep 0, 8
+	sound_ret
