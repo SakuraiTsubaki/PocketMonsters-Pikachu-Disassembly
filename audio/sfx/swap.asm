7853c5cd93f8c866b755b5df1c7da971e8cde044
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:44:17 +0900

    audio: reconstruct Swap SFX

---
+++ audio/sfx/swap.asm
@@ -0,0 +1,12 @@
+	audio_eng_channel SFX_Swap, 5
+
+	duty_cycle 2
+	square_note 8, 14, 1, 1856
+	sound_ret
+
+	audio_eng_channel SFX_Swap, 6
+
+	duty_cycle 2
+	square_note 2, 0, 8, 0
+	square_note 8, 11, 1, 1857
+	sound_ret
