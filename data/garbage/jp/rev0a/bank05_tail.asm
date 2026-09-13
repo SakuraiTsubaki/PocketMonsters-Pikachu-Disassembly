d276094fdf25ed9947c2e77cf137b456ea7ac688
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 13:56:55 +0900

    bank05: preserve JP Rev 0A Garbage 5 tail

---
+++ data/garbage/jp/rev0a/bank05_tail.asm
@@ -0,0 +1,26 @@
+; Pokémon Yellow JP Bank 05 historical Garbage 5 tail (jp-rev0a).
+; ROM offsets 0x17E8A-0x17FFF, 374 bytes.
+	db $E7, $FF, $F5, $EF, $5F, $FF, $CF, $77, $FE, $7F, $FB, $FC, $F5, $BE, $CF, $FB
+	db $F6, $DF, $FE, $FF, $FF, $CF, $67, $FE, $FF, $FB, $BB, $F9, $B4, $FF, $EF, $FB
+	db $7F, $7F, $9F, $FB, $FF, $BF, $FF, $EE, $6F, $DD, $F7, $35, $FD, $FD, $FD, $7F
+	db $FF, $7F, $7F, $FF, $9F, $F1, $FD, $F4, $FE, $EF, $BD, $FD, $EF, $FB, $FA, $ED
+	db $FB, $FF, $FF, $BB, $FB, $FF, $FE, $EB, $FF, $FD, $BD, $DB, $FF, $FF, $7F, $6F
+	db $9F, $DE, $3F, $EF, $F7, $7F, $7E, $FF, $DF, $5F, $FD, $3B, $FB, $DE, $73, $FF
+	db $DE, $48, $FE, $FF, $FF, $BF, $FF, $E6, $D3, $FF, $F5, $DE, $3B, $FF, $FD, $F5
+	db $C7, $76, $F7, $E4, $7F, $F6, $5E, $D5, $AE, $C4, $2F, $B6, $03, $0C, $DE, $2C
+	db $A1, $65, $83, $8E, $F3, $55, $C4, $49, $6F, $8D, $87, $59, $C3, $52, $04, $E6
+	db $F6, $BF, $29, $A9, $3E, $A8, $3D, $39, $B9, $89, $AD, $D1, $2A, $43, $7A, $58
+	db $A5, $48, $15, $4E, $D3, $84, $6A, $29, $B3, $95, $C7, $C3, $9A, $46, $7B, $30
+	db $3D, $5D, $E5, $C7, $2D, $A8, $62, $D5, $9C, $97, $69, $A4, $21, $15, $D4, $00
+	db $22, $57, $78, $41, $00, $28, $CC, $F6, $18, $80, $00, $07, $1B, $0C, $7D, $8E
+	db $84, $66, $95, $4C, $8A, $99, $EF, $CE, $3A, $D3, $15, $A3, $25, $9D, $B3, $8A
+	db $63, $46, $BF, $E9, $7D, $29, $43, $13, $B6, $05, $1D, $99, $1A, $C9, $1B, $4F
+	db $75, $70, $C8, $E3, $59, $0C, $BF, $FB, $3A, $F7, $CB, $FD, $FE, $5E, $EF, $BB
+	db $37, $FD, $7D, $DF, $66, $FC, $FF, $BF, $FB, $7F, $EA, $DF, $FF, $F6, $DC, $BD
+	db $CF, $5F, $D7, $FB, $3C, $AE, $FF, $FD, $7A, $AF, $FC, $FD, $BB, $FF, $EF, $FD
+	db $3B, $FB, $BF, $FF, $FF, $BF, $7B, $FF, $DE, $3F, $FB, $DE, $F6, $BC, $3F, $3B
+	db $BF, $FF, $EF, $9D, $DF, $FC, $37, $FF, $BF, $FF, $F9, $F7, $F5, $ED, $FB, $FF
+	db $7F, $7F, $FF, $EF, $7D, $FF, $7E, $FF, $FF, $FC, $9B, $BF, $FF, $F7, $AF, $FF
+	db $FD, $2F, $77, $7F, $3D, $FB, $E0, $FF, $7F, $F7, $DF, $7E, $D3, $FD, $7B, $EE
+	db $D7, $3F, $FE, $6D, $FA, $EB, $D7, $E7, $FF, $BF, $B5, $F3, $FA, $F7, $DB, $F3
+	db $E5, $FF, $DB, $EF, $FE, $DF
