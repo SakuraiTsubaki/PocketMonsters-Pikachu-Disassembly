a1bfc3896eebbf357b8ffa503a9a276afe00e98d
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 10:09:05 +0900

    audio: preserve JP Rev 0A Bank 02 garbage tail as ASM

---
+++ data/garbage/jp/rev0a/bank02_tail.asm
@@ -0,0 +1,26 @@
+; Pocket Monsters Pikachu JP Rev 0A — Bank 02 historical Garbage 2
+; ROM range: $BEC7-$BFFF (313 bytes)
+; Raw SHA-1: 8b7ee3d3c414c9b31c846f39c5f83dd2324486cb
+; Git blob SHA-1: 72dd62ff37238e91d7b5e990b248c42b84d11a55
+; Preserved verbatim from the verified reference ROM; do not normalize as padding.
+
+	db $ef, $ef, $2f, $28, $cb, $f6, $71, $57, $d4, $b6, $fd, $9d, $c2, $17, $bf, $bf
+	db $77, $37, $2e, $fc, $cb, $f7, $da, $67, $d2, $7f, $f9, $bf, $ff, $fd, $bd, $d5
+	db $ff, $fd, $f6, $ff, $cf, $f6, $f8, $ff, $ab, $eb, $ef, $d7, $72, $f4, $76, $d7
+	db $f1, $9f, $f7, $ff, $e7, $b5, $cb, $c3, $d2, $ad, $90, $80, $d2, $e5, $12, $0c
+	db $82, $00, $5f, $15, $82, $23, $f5, $15, $05, $cd, $0c, $ae, $0d, $ed, $11, $04
+	db $61, $70, $30, $40, $01, $c4, $20, $8c, $14, $97, $26, $24, $f9, $6e, $0b, $c1
+	db $01, $39, $25, $0b, $b0, $45, $1c, $44, $c5, $aa, $00, $04, $91, $14, $43, $09
+	db $13, $04, $13, $e9, $18, $43, $10, $14, $10, $13, $65, $06, $70, $48, $b3, $64
+	db $00, $81, $10, $a0, $03, $86, $1c, $0b, $95, $02, $85, $55, $8b, $c1, $80, $84
+	db $59, $b4, $21, $04, $34, $1d, $62, $00, $0e, $19, $9c, $cd, $00, $41, $2f, $1c
+	db $95, $a3, $98, $23, $36, $60, $24, $a0, $10, $90, $52, $fd, $08, $6f, $91, $02
+	db $21, $f0, $21, $93, $42, $c2, $10, $41, $78, $ff, $d7, $73, $c7, $fe, $af, $df
+	db $fd, $d9, $bf, $db, $df, $f5, $9f, $df, $dd, $6b, $c7, $df, $f1, $f3, $ff, $fb
+	db $73, $3e, $f7, $fd, $d5, $fb, $0f, $77, $41, $78, $a4, $72, $76, $ba, $f7, $bf
+	db $40, $f7, $fd, $6f, $ff, $dc, $be, $3b, $fd, $79, $4c, $7f, $bd, $fa, $e9, $ae
+	db $e7, $3f, $bf, $7c, $df, $ef, $fd, $2b, $55, $b7, $f7, $bf, $ae, $fe, $39, $be
+	db $cf, $bf, $7f, $bf, $7d, $9f, $c7, $77, $78, $bf, $8a, $bf, $ef, $7c, $5f, $ff
+	db $19, $bf, $ff, $ff, $dd, $f9, $ef, $fd, $f1, $d9, $bc, $bb, $ff, $ff, $7f, $d9
+	db $b3, $ff, $f5, $dc, $ff, $7e, $fd, $f7, $d5, $be, $bf, $fe, $b3, $15, $fe, $ff
+	db $1f, $cc, $95, $af, $59, $fd, $fa, $ff, $f2
