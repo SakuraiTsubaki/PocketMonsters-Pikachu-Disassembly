c6c22e56bc7392fe10a393cb1bce228baa2e6930
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:46:24 +0900

    audio: reconstruct Poisoned SFX

---
+++ audio/sfx/poisoned.asm
@@ -0,0 +1,9 @@
+	audio_eng_channel SFX_Poisoned, 5
+
+	duty_cycle 0
+	pitch_sweep 1, 4
+	square_note 4, 15, 2, 1536
+	sound_loop 4, SFX_Poisoned_{d:audio_engine}_Ch5
+	square_note 15, 15, 3, 1536
+	pitch_sweep 0, 8
+	sound_ret
