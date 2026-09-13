ffb80746a673db212aa8fb27b78fadca678e419b
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:35:39 +0900

    bank02: add shared engine 1 and 3 SFX headers

---
+++ audio/headers/sfx_headers_1_3.asm
@@ -0,0 +1,38 @@
+; Shared SFX pointers used by audio engines 1 and 3.
+
+audio_header_eng SFX_Get_Item1,        5, 6, 7
+audio_header_eng SFX_Get_Item2,        5, 6, 7
+audio_header_eng SFX_Tink,             5
+audio_header_eng SFX_Heal_HP,          5
+audio_header_eng SFX_Heal_Ailment,     5
+audio_header_eng SFX_Start_Menu,       8
+audio_header_eng SFX_Press_AB,         5
+audio_header_eng SFX_Pokedex_Rating,   5, 6, 7
+audio_header_eng SFX_Get_Key_Item,     5, 6, 7
+audio_header_eng SFX_Poisoned,         5
+audio_header_eng SFX_Trade_Machine,    5
+audio_header_eng SFX_Turn_On_PC,       5
+audio_header_eng SFX_Turn_Off_PC,      5
+audio_header_eng SFX_Enter_PC,         5
+audio_header_eng SFX_Shrink,           5
+audio_header_eng SFX_Switch,           5
+audio_header_eng SFX_Healing_Machine,  5
+audio_header_eng SFX_Teleport_Exit1,   5
+audio_header_eng SFX_Teleport_Enter1,  5
+audio_header_eng SFX_Teleport_Exit2,   5
+audio_header_eng SFX_Ledge,            5
+audio_header_eng SFX_Teleport_Enter2,  8
+audio_header_eng SFX_Fly,              8
+audio_header_eng SFX_Denied,           5, 6
+audio_header_eng SFX_Arrow_Tiles,      5
+audio_header_eng SFX_Push_Boulder,     8
+audio_header_eng SFX_SS_Anne_Horn,     5, 6
+audio_header_eng SFX_Withdraw_Deposit, 5
+audio_header_eng SFX_Cut,              8
+audio_header_eng SFX_Go_Inside,        8
+audio_header_eng SFX_Swap,             5, 6
+audio_header_eng SFX_59,               5, 6
+audio_header_eng SFX_Purchase,         5, 6
+audio_header_eng SFX_Collision,        5
+audio_header_eng SFX_Go_Outside,       8
+audio_header_eng SFX_Save,             5, 6
