b1e3c21465422afc6bbb4e5a89420663384a6af3
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:45:08 +0900

    audio: reconstruct Purchase SFX

---
+++ audio/sfx/purchase.asm
@@ -0,0 +1,14 @@
+	audio_eng_channel SFX_Purchase, 5
+
+	duty_cycle 2
+	square_note 4, 14, 1, 1792
+	square_note 8, 15, 2, 2016
+	sound_ret
+
+	audio_eng_channel SFX_Purchase, 6
+
+	duty_cycle 2
+	square_note 1, 0, 8, 0
+	square_note 4, 9, 1, 1729
+	square_note 8, 10, 2, 1953
+	sound_ret
