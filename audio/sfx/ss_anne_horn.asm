f5497c148663e6013a40e44e4431ddf71b5f9bc6
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:50:09 +0900

    audio: reconstruct SS Anne Horn SFX

---
+++ audio/sfx/ss_anne_horn.asm
@@ -0,0 +1,23 @@
+	audio_eng_channel SFX_SS_Anne_Horn, 5
+
+	duty_cycle 2
+	square_note 15, 15, 0, 1280
+	square_note 4, 0, 0, 0
+	square_note 15, 15, 0, 1280
+	square_note 15, 15, 0, 1280
+	square_note 15, 15, 0, 1280
+	square_note 15, 15, 0, 1280
+	square_note 15, 15, 2, 1280
+	sound_ret
+
+	audio_eng_channel SFX_SS_Anne_Horn, 6
+
+	duty_cycle 3
+	square_note 15, 15, 0, 1154
+	square_note 4, 0, 0, 0
+	square_note 15, 15, 0, 1154
+	square_note 15, 15, 0, 1154
+	square_note 15, 15, 0, 1154
+	square_note 15, 15, 0, 1154
+	square_note 15, 15, 2, 1154
+	sound_ret
