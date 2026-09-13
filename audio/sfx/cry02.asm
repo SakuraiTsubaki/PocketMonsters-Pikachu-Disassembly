d498d4b30da9da91e9daf13f35d118d43f5bac90
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 10:16:50 +0900

    audio: add fourth Bank 02 cry source batch

---
+++ audio/sfx/cry02.asm
@@ -0,0 +1,20 @@
+	audio_eng_channel SFX_Cry02, 5
+
+	duty_cycle 0
+	square_note 8, 15, 5, 1152
+	square_note 2, 14, 1, 1504
+	square_note 8, 13, 1, 1500
+	sound_ret
+
+
+	audio_eng_channel SFX_Cry02, 6
+
+	duty_cycle_pattern 2, 2, 1, 1
+	square_note 7, 9, 5, 1089
+	square_note 2, 8, 1, 1313
+	square_note 8, 6, 1, 1306
+
+
+	audio_eng_channel SFX_Cry02, 8
+
+	sound_ret
