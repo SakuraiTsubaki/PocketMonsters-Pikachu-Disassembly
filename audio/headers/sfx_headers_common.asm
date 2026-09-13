3ff05466e8d9a4e55c4029cd81fe65f86b6efe45
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 09:35:29 +0900

    bank02: add shared SFX header table

---
+++ audio/headers/sfx_headers_common.asm
@@ -0,0 +1,62 @@
+; Shared SFX header entries used by multiple audio engines.
+	audio_header_eng SFX_Headers
+	db $ff, $ff, $ff
+
+	audio_header_eng SFX_Noise_Instrument01, 8
+	audio_header_eng SFX_Noise_Instrument02, 8
+	audio_header_eng SFX_Noise_Instrument03, 8
+	audio_header_eng SFX_Noise_Instrument04, 8
+	audio_header_eng SFX_Noise_Instrument05, 8
+	audio_header_eng SFX_Noise_Instrument06, 8
+	audio_header_eng SFX_Noise_Instrument07, 8
+	audio_header_eng SFX_Noise_Instrument08, 8
+	audio_header_eng SFX_Noise_Instrument09, 8
+	audio_header_eng SFX_Noise_Instrument10, 8
+	audio_header_eng SFX_Noise_Instrument11, 8
+	audio_header_eng SFX_Noise_Instrument12, 8
+	audio_header_eng SFX_Noise_Instrument13, 8
+	audio_header_eng SFX_Noise_Instrument14, 8
+	audio_header_eng SFX_Noise_Instrument15, 8
+	audio_header_eng SFX_Noise_Instrument16, 8
+	audio_header_eng SFX_Noise_Instrument17, 8
+	audio_header_eng SFX_Noise_Instrument18, 8
+	audio_header_eng SFX_Noise_Instrument19, 8
+
+	audio_header_eng SFX_Cry00, 5, 6, 8
+	audio_header_eng SFX_Cry01, 5, 6, 8
+	audio_header_eng SFX_Cry02, 5, 6, 8
+	audio_header_eng SFX_Cry03, 5, 6, 8
+	audio_header_eng SFX_Cry04, 5, 6, 8
+	audio_header_eng SFX_Cry05, 5, 6, 8
+	audio_header_eng SFX_Cry06, 5, 6, 8
+	audio_header_eng SFX_Cry07, 5, 6, 8
+	audio_header_eng SFX_Cry08, 5, 6, 8
+	audio_header_eng SFX_Cry09, 5, 6, 8
+	audio_header_eng SFX_Cry0A, 5, 6, 8
+	audio_header_eng SFX_Cry0B, 5, 6, 8
+	audio_header_eng SFX_Cry0C, 5, 6, 8
+	audio_header_eng SFX_Cry0D, 5, 6, 8
+	audio_header_eng SFX_Cry0E, 5, 6, 8
+	audio_header_eng SFX_Cry0F, 5, 6, 8
+	audio_header_eng SFX_Cry10, 5, 6, 8
+	audio_header_eng SFX_Cry11, 5, 6, 8
+	audio_header_eng SFX_Cry12, 5, 6, 8
+	audio_header_eng SFX_Cry13, 5, 6, 8
+	audio_header_eng SFX_Cry14, 5, 6, 8
+	audio_header_eng SFX_Cry15, 5, 6, 8
+	audio_header_eng SFX_Cry16, 5, 6, 8
+	audio_header_eng SFX_Cry17, 5, 6, 8
+	audio_header_eng SFX_Cry18, 5, 6, 8
+	audio_header_eng SFX_Cry19, 5, 6, 8
+	audio_header_eng SFX_Cry1A, 5, 6, 8
+	audio_header_eng SFX_Cry1B, 5, 6, 8
+	audio_header_eng SFX_Cry1C, 5, 6, 8
+	audio_header_eng SFX_Cry1D, 5, 6, 8
+	audio_header_eng SFX_Cry1E, 5, 6, 8
+	audio_header_eng SFX_Cry1F, 5, 6, 8
+	audio_header_eng SFX_Cry20, 5, 6, 8
+	audio_header_eng SFX_Cry21, 5, 6, 8
+	audio_header_eng SFX_Cry22, 5, 6, 8
+	audio_header_eng SFX_Cry23, 5, 6, 8
+	audio_header_eng SFX_Cry24, 5, 6, 8
+	audio_header_eng SFX_Cry25, 5, 6, 8
